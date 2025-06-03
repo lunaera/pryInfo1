23771a2b14a7d675ac6e21e0344fc918a277fcf3
unknown <erahinpachecoluna@gmail.com>
2025-06-03 11:26:31 -0600

    cambios en el script2

--- js/script2.js
+++ js/script2.js
@@ -10,3 +10,5 @@ console.log("hice cambios en el archivo script2.js");
 
 
 console.log("hice cambios en el archivo script2.js");
+console.log("hice cambios en el archivo script2.js");
+console.log("hice cambios en el archivo script2.js");
