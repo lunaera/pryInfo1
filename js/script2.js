eafbfc4960eecd25615af90d703e77482d28490e
unknown <erahinpachecoluna@gmail.com>
2025-06-03 09:55:55 -0600

    programa array edades erahin

--- js/script2.js
+++ js/script2.js
@@ -1,3 +1,6 @@
 
 
-console.log("hola mundo desde script2.js");
+const arayEdades=[18, 25, 30, 45, 60];
+console.log(arayEdades);
+console.log(arayEdades.filter((edad) => edad >= 30));
+console.log(arayEdades.map((edad) => edad + 1));
