11e2ca9d7452972999e01ac1ad7c7ab3a93e3a68
unknown <erahinpachecoluna@gmail.com>
2025-06-03 13:22:20 -0600

    cambios

--- js/script3.js
+++ js/script3.js
@@ -2,8 +2,11 @@ console.log("hice cambios en el archivo script3.js");
 
 // tarea 3 calificada
 
-console.log("el usuario 2 hizo cmabios");
+console.log("el usuario 2 hizo cambios");
 console.log("soy el propietario e hice cambios");
 
 console.log(5+8);
 // cambios desde el usuario 2
+
+console.log(9-8);
+// cambios desde el usuario luna
