392dc934caade19d6dea538f3ab8faf4148a2408
unknown <erahinpachecoluna@gmail.com>
2025-06-03 16:02:42 -0600

    subiendo cambios

--- js/script2.js
+++ js/script2.js
@@ -5,13 +5,4 @@ console.log(arayEdades);
 console.log(arayEdades.filter((edad) => edad >= 30));
 console.log(arayEdades.map((edad) => edad + 1));
 
-
-console.log("hice cambios en el archivo script2.js");
-
-// Tarea calificada nuevamente
-
-
-// Tarea calificada nuevamente
-
-
-console.log("hice cambios nuevamente el ultimo");
+// hice cambios en el archivo script2.js para subirlo a la nueva rama main2
