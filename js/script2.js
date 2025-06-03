c8eb55803d5c995967b2a703ee64a64981de3a7e
unknown <erahinpachecoluna@gmail.com>
2025-06-03 10:21:41 -0600

    cambios en el script2

--- js/script2.js
+++ js/script2.js
@@ -5,6 +5,4 @@ console.log(arayEdades);
 console.log(arayEdades.filter((edad) => edad >= 30));
 console.log(arayEdades.map((edad) => edad + 1));
 console.log("hice cambios en el archivo script2.js");
-console.log("hice cambios en el archivo script2.js");
-console.log("hice cambios en el archivo script2.js");
 
