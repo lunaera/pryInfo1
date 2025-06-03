0bb37ceb0e6fbd723643fafec4810fb0580e231f
lunaera <erahinpachecoluna@gmail.com>
2025-06-03 13:12:38 -0600

    nuevos cambios en script 3

--- js/script3.js
+++ js/script3.js
@@ -5,5 +5,7 @@ console.log("hice cambios en el archivo script3.js");
 console.log("el usuario 2 hizo cmabios");
 console.log("soy el propietario e hice cambios");
 
-console.log(5+8);
+console.log(5 + 8);
 // cambios desde el usuario 2
+
+// estoy haciendo cambios desde el anfitrión al archivo script3.js
