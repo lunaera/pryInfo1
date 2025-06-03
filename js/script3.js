6075db4b8f7f288b30018d7e453898d3e0511402
unknown <erahinpachecoluna@gmail.com>
2025-06-03 13:04:46 -0600

    cambios en script 3 localmente

--- js/script3.js
+++ js/script3.js
@@ -4,3 +4,6 @@ console.log("hice cambios en el archivo script3.js");
 
 console.log("el usuario 2 hizo cmabios");
 console.log("soy el propietario e hice cambios");
+
+console.log(5+8);
+// cambios desde el usuario 2
