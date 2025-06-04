aa4c522593fac592aaa2c57ec3b7dd78691d246d
lunaera <erahinpachecoluna@gmail.com>
2025-06-04 10:46:10 -0600

    scripts eliminados

--- js/script2.js
+++
@@ -1,8 +0,0 @@
-
-
-const arayEdades = [18, 25, 30, 45, 60];
-console.log(arayEdades);
-console.log(arayEdades.filter((edad) => edad >= 30));
-console.log(arayEdades.map((edad) => edad + 1));
-
-// hice cambios en el archivo script2.js para subirlo a la nueva rama main2
